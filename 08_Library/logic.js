122be06cd2170679302cbc45848fb512badc4c86
RuiShengGit <79069843+RuiShengGit@users.noreply.github.com>
2026-01-08 08:18:06 +0800

    feat: Add theme to the page

--- 08_Library/logic.js
+++ 08_Library/logic.js
@@ -1,3 +1,32 @@
+const light = document.getElementById('light');
+const dark = document.getElementById('dark');
+const body = document.body;
+
+window.addEventListener('load', checkTheme);
+
+
+function setTheme(theme) {
+    body.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    checkTheme();
+}
+
+function checkTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    console.log(savedTheme);
+    body.setAttribute('data-theme', savedTheme);
+}
+
+light.addEventListener('click', () => {
+    console.log("light mode");
+    setTheme('light');
+});
+
+dark.addEventListener('click', () => {
+    console.log("dark mode");
+    setTheme('dark');
+});
+
 const myLibrary = [];
 
 function Book(title, author,pages,read){
